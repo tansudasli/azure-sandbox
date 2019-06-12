a6a778a7a6d01448884c635df5543594a255b5aa
Tansu Dasli <tansudasli@gmail.com>
2019-06-12 12:23:32 +0300

    switched 2 az cli (removed resource manager templates)

--- iaas-prod-ready/01-network-resources.deploy.sh
+++
@@ -1,7 +0,0 @@
-echo "Enter the Resource Group name:" &&
-read resourceGroupName &&
-echo "Enter the location (i.e. westeurope):" &&
-read location &&
-
-az group create -n $resourceGroupName --l "$location" &&
-az group deployment create -g $resourceGroupName --template-file "$HOME/azuredeploy.json"
